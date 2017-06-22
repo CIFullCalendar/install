06e73634e472ea9b741a575d555f8dd9e6def721
Andre Gardiner <andre@sirdre.com>
2017-06-22 17:57:25 -0600

    Update database.php

--- database.php
+++ database.php
@@ -193,7 +193,7 @@ if($_POST) {
 		</form> 
  
 	<!-- jQuery & JavaScript -->  
-	<script src="./assets/lib/js/jquery-1.11.1.min.js"></script>
+	<script src="./assets/js/jquery-1.11.1.min.js"></script>
 	<script src="./assets/js/jquery.validate.min.js"></script>
 	<script src="./assets/js/form-validation.js"></script> 
 
@@ -208,7 +208,7 @@ if($_POST) {
 	<script src="./assets/js/sidebar.js"></script> 		
      </div>
       <footer class="doc-footer"> 
-		<p>CIFullcalendar v3 - created and maintained by <a href="https://www.sirdre.com">Sir.Dre</a></p> 
+		<p>CIFullcalendar v2 - created and maintained by <a href="https://www.sirdre.com">Sir.Dre</a></p> 
       </footer>
     </div>
   </div>
